fd08831b9e61dfd7b030b80685d989bdb81e7dc6
Michał Śmiech <michal.smiech@mlabs.pl>
2021-10-31 22:54:47 +0100

    Add firebase

--- app/src/main/java/com/michasoft/thelasttime/MainActivity.kt
+++ app/src/main/java/com/michasoft/thelasttime/MainActivity.kt
@@ -2,6 +2,8 @@ package com.michasoft.thelasttime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
